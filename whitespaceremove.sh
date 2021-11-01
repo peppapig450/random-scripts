0ea6abac78c24aae043f62c81bead6d493f3d24b
peppapig450 <nbgenius1@gmail.com>
2021-10-31 17:46:26 -0700

    lol

--- whitespaceremove.sh
+++ whitespaceremove.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SAVEIFS=$IFS
+SAVEIFS=$' \t\n'
 IFS=$(echo -en "\n\b")
 for oldname in $(find "$HOME/.local/share/fonts/" -type f) 
 do 
   newname=$(echo $oldname | sed -e 's/ //g') 
-  mv -i "$oldname" "$newname"
+  mv -i "$oldname" "$newname" 2> /dev/null
 done
 IFS=$SAVEIFS

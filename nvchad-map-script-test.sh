721244b9f8498c025733dd562adfdca974d51cd3
peppapig450 <nbgenius1@gmail.com>
2021-11-02 19:37:40 -0700

    work on nvchad map script some more

--- nvchad-map-script-test.sh
+++ nvchad-map-script-test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 
-set -x 
-set -v
-temp_file=$(mktemp)
-trap "rm -f $temp_file" 0 2 3 15
-for f in $(find "$HOME/.config/nvim/lua/plugins/configs/" -type f); do awk '/keymap/ {print $2,$4}' $f | column -s ',' -t >> tempfile; done 
-cat tempfile 
-rm ${temp_file}
+rm -r map 
+for f in $(find "$HOME/.config/nvim/lua/plugins/configs/" -type f); do awk '/keymap/ {print $2,$4}' "$f" | tr -d "()" | sed 's/<CR>"/ /' | tail -n +4 |  column -s ',' -t >> map; done 
+# sed -i 's/print(vim.inspect(/ /' map 
+# sed -i 's/()<CR>"/ /' map 
+

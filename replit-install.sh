488fd91a1091da6ef7e2a6e9494e3cce0b3cd9a5
Nick Brady <55930078+peppapig450@users.noreply.github.com>
2021-12-16 04:04:18 +0000

    j

--- replit-install.sh
+++ replit-install.sh
@@ -5,9 +5,11 @@ set -Eeuo pipefail
 
 _pkgname=exa-0.10.0
 _source_url=https://github.com/ogham/exa/archive/refs/tags/v0.10.0.tar.gz
+_srcdir=/home/runner/random-scripts/exa-0.10.0
+_pkgdir=/home/runner/.local/bin/
 
 test() {
-    local -a commands=("rustc" "cargo" "curl")
+    local -a commands=("rustc" "cargo" "curl" "unzip")
     for dep in "${commands[@]}"; do 
         if ! command -v "$dep" >/dev/null 2>&1; then 
             echo -e "$dep is not installed or is not on your path!\nExiting now."
@@ -18,7 +20,7 @@ test() {
 
 prepare() {
     if [[ $(pwd -P) != $HOME ]]; then 
-        cd && curl -LJO "$_source_url"
+        curl -LJO "$_source_url"
         if [[ -f "$_pkgname.zip" ]]; then 
             unzip "$_pkgname.zip"
         elif [[ -f "$_pkgname.tar.gz" ]]; then 
@@ -32,16 +34,34 @@ prepare() {
             tar -xf "$_pkgname.tar.gz"
         fi
     fi
+    cd $_pkgname
+}
+
+build() {
+    export RUSTFLAGS="-C opt-level=2 -C target-cpu=native"
+    local dirs=$(pwd -P)
+    if [[ $(basename $dirs) = 'exa-0.10.0' ]]; then 
+        cargo build --release
+    fi
+}
+
+install() {
+    cd $_srcdir
+    mkdir -p ~/.local/bin 
+    cp target/release/exa "$_pkgdir"
+    echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
 }
 
 cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  if [[ $(pwd -P) =~ 'exa' ]]; then 
-    if [[ -d ./target ]]; then 
-        rm -rf target/ 
-    fi
-  fi 
-  echo "Cleaning up target build dir"
+    find ~/ -type f -name '*.tar.gz' -exec rm {} \;
 }
 
-test && prepare
+run() {
+    test 
+    prepare
+    build
+    install
+    cleanup
+}
+
+run 

bcce9b03520b52caaf5fc96dd944a00cefe942d4
peppapig450 <nbgenius1@gmail.com>
2021-12-18 16:00:28 -0800

    add back examples

---
+++ examples/bash-getopts-example.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# super simple getopts example
+directory="$HOME"
+depth=123
+while getopts "hd:l" opt; do
+  case $opt in
+    d ) directory=$OPTARG;;
+    l ) depth=$OPTARG;;
+    h ) usage
+    exit 0;;
+    * ) usage
+    exit 1;;
+esac
+done
+echo "$directory $depth"
